90d9a58bf8153cc92ccc09e9e63f6651334bbcc0
GammaNu/Millicent Billette <commiter@1000i100.fr>
2014-09-09 05:31:20 +0200

    version webui

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
-var batiments = {
+var MAX_RESULT = 20;
+var TYPES_MAISONS = {
 	'Hutte':14,
 	'Maison sur pilotis':22,
 	'Chaumière':27,
@@ -10,7 +11,7 @@ var batiments = {
 	'Maison à charpente':67,
 	'Maison de ville':156,
 	'Maison en grès brun':94,
-	'Appartements':205,
+	'Appartement':205,
 	'Propriété':123,
 	'Maison de campagne':207,
 	'Maison à arcade':155,
@@ -27,23 +28,66 @@ var batiments = {
 	'Maison à deux appartements':1040,
 	"Tour d'habitation en préfabriqué":2120,
 	'Pavillon':1150
-}
+};
+var PLURIALS = {
+	'Hutte':'Huttes',
+	'Maison sur pilotis':'Maisons sur pilotis',
+	'Chaumière':'Chaumières',
+	'Chalet':'Chalets',
+	'Maison':'Maisons',
+	'Maisonnette':'Maisonnettes',
+	'Maison en bois':'Maisons en bois',
+	'Maison à charpente':'Maisons à charpentes',
+	'Maison de ville':'Maisons de ville',
+	'Maison en grès brun':'Maisons en grès brun',
+	'Appartement':'Appartements',
+	'Propriété':'Propriétés',
+	'Maison de campagne':'Maisons de campagne',
+	'Maison à arcade':'Maisons à arcades',
+	'Maison à mansarde':'Maisons à mansardes',
+	'Pension':'Pensions',
+	'Maison victorienne':'Maisons victoriennes',
+	"Maison d'ouvrier":"Maisons d'ouvriers",
+	'Logement social':'Logements sociaux',
+	'Immeuble':'Immeubles',
+	"Tour d'habitation":"Tours d'habitation",
+	'Motel':'Motels',
+	'Maison en préfabriqué':'Maisons en préfabriqués',
+	'Maison de banlieue':'Maisons de banlieue',
+	'Maison à deux appartements':'Maisons à deux appartements',
+	"Tour d'habitation en préfabriqué":"Tours d'habitation en préfabriqué",
+	'Pavillon':'Pavillons'
+
+};
 function Solver(target,incrementAllowed){
 	var self = this;
 	self.target = target;
 	self.incrementAllowed = incrementAllowed;
 	self.increments = {};
+	self.reponses = [];
 	initIncrements();
 
 	try{
 		while(1) incrementIncrements();
 	} catch(e){
-		if (e == 'All possibility have been tried.') console.log(e);
+		if (e == 'All possibility have been tried.'){
+			if(self.reponses.length) self.reponses.push("Toutes les solutions possibles ont été trouvées.");
+			else self.reponses.push("Aucune solution n'est possible sans réduire préalablement votre population actuelle.");
+		}
+		else if (e == 'Too many possibility'){
+			self.reponses.push("Seules les premières solutions ont été affichées, augmentez votre population actuelle pour réduire le nombre de possibilité.");
+		}
 		else throw e;
 	}
+	var resultNode = document.getElementById('result');
+	var last = self.reponses.pop();
+	if(self.reponses.length==1) resultNode.innerHTML = 'Solution trouvée :<br/>'+ self.reponses[0]+'<br/>'+last;
+	if(self.reponses.length>1) resultNode.innerHTML = 'Plusieurs solutions ont été trouvées :<br/><ul><li>'+ self.reponses.join('</li><li>')+'</li></ul>'+last;
+	if(!self.reponses.length) resultNode.innerHTML = last;
 
 
-	function initIncrements(){
+
+		function initIncrements(){
 		for (var key in self.incrementAllowed) {
 			self.increments[key]=0;
 		}
@@ -52,7 +96,7 @@ function Solver(target,incrementAllowed){
 		for (var key in self.incrementAllowed) {
 			self.increments[key]++;
 			var sum = sumIncrements();
-			if (sum==self.target) displaySolution();
+			if (sum==self.target) addSolution();
 			if (sum<=self.target) return;
 			else self.increments[key] = 0;
 		}
@@ -65,12 +109,20 @@ function Solver(target,incrementAllowed){
 		}
 		return sum;
 	}
-	function displaySolution(){
-		var message = 'Solution found : '+self.target+' =';
+	function addSolution(){
+		if(self.reponses.length>=MAX_RESULT) throw 'Too many possibility';
+		var message = [];
 		for (var key in self.incrementAllowed) {
-			if (self.increments[key]) message += ' '+self.increments[key]+'*'+self.incrementAllowed[key]+'('+key+')';
+			if (self.increments[key]>1) message.push('<strong>'+self.increments[key]+' '+PLURIALS[key]+'</strong>');
+			else if (self.increments[key]==1) message.push('<strong>'+self.increments[key]+' '+key+'</strong>');
 		}
-		console.log(message);
+		var last = message.pop();
+		self.reponses.push(message.join(', ')+' et '+last);
 	}
 }
-new Solver(6750-6715+155+207-285,batiments);
+function solve(){
+	var actualPopulation = document.getElementById('actual').value;
+	var targetPopulation = document.getElementById('target').value;
+	new Solver(targetPopulation-actualPopulation,TYPES_MAISONS);
+}
+
